1679b8a6945eee66a9ab7af6c073db6e5247180f
Dmitx <37021407+Dmitx@users.noreply.github.com>
2018-05-11 21:35:30 +0300

    Added reset password for user

--- api/routes/user.js
+++ api/routes/user.js
@@ -8,6 +8,8 @@ router.post("/signup", UserController.user_signup);
 
 router.post("/login", UserController.user_login);
 
+router.patch("/reset_password/:userId", checkAuth, UserController.user_reset_password);
+
 router.delete("/:userId", checkAuth, UserController.user_delete);
 
 module.exports = router;
