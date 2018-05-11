ab8db3d4a08dde0d3344aa2319ce0b516fc9f8bc
Dmitx <37021407+Dmitx@users.noreply.github.com>
2018-05-11 21:43:15 +0300

    Added comment

--- api/routes/user.js
+++ api/routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const UserController = require('../controllers/user');
 const checkAuth = require('../middleware/check-auth');
 
+// Handle incoming requests
 router.post("/signup", UserController.user_signup);
 
 router.post("/login", UserController.user_login);
