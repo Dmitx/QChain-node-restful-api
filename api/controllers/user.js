144656a2990992286516aaf57bc9662416cc3540
Dmitx <37021407+Dmitx@users.noreply.github.com>
2018-05-12 16:16:53 +0300

    added username and name for user

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,41 +5,53 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 exports.user_signup = (req, res, next) => {
-  User.find({ email: req.body.email })
+  User.find({ username: req.body.username })
     .exec()
     .then(user => {
       if (user.length >= 1) {
         return res.status(409).json({
-          message: "Mail exists"
+          message: "Username exists"
         });
       } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              error: err
-            });
-          } else {
-            const user = new User({
-              _id: new mongoose.Types.ObjectId(),
-              email: req.body.email,
-              password: hash
-            });
-            user
-              .save()
-              .then(result => {
-                console.log(result);
-                res.status(201).json({
-                  message: "User created"
-                });
-              })
-              .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                  error: err
-                });
+        User.find({ email: req.body.email })
+          .exec()
+          .then(user => {
+            if (user.length >= 1) {
+              return res.status(409).json({
+                message: "Mail exists"
               });
-          }
-        });
+            } else {
+              bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err) {
+                  return res.status(500).json({
+                    error: err
+                  });
+                } else {
+                  const user = new User({
+                    _id: new mongoose.Types.ObjectId(),
+                    email: req.body.email,
+                    username: req.body.username,
+                    name: req.body.name,
+                    password: hash
+                  });
+                  user
+                    .save()
+                    .then(result => {
+                      console.log(result);
+                      res.status(201).json({
+                        message: "User created"
+                      });
+                    })
+                    .catch(err => {
+                      console.log(err);
+                      res.status(500).json({
+                        error: err
+                      });
+                    });
+                }
+              });
+            }
+          });
       }
     });
 };
