93683b453c34327ccc2bfec166b0b6855119f860
Dmitx <37021407+Dmitx@users.noreply.github.com>
2018-05-12 12:47:16 +0300

    adjusted comment

--- api/routes/contracts.js
+++ api/routes/contracts.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const ContractsController = require('../controllers/contracts');
 const checkAuth = require('../middleware/check-auth');
 
-// Handle incoming GET requests to /orders
+// Handle incoming requests
 router.get("/:userId", checkAuth, ContractsController.contracts_get_user_all);
 
 router.post("/:userId", checkAuth, ContractsController.contracts_create_contract);
